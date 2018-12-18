ff36ffa14d69630320b524856688da6383945539
dennys0029 <dennys0029@gmail.com>
2018-12-18 08:31:19 -0500

    agregamos un label

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,20 @@ namespace github
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mensaje Git");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
